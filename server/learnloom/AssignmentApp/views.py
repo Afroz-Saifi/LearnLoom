6c2e8ccbd91b0c091858b012a7bcdcb05eb463b0
Afroz-Saifi <afrozsaifi810@gmail.com>
2023-09-05 15:27:37 +0530

    created get route for assignments based on course and start date and enroll date

--- server/learnloom/AssignmentApp/views.py
+++ server/learnloom/AssignmentApp/views.py
@@ -5,6 +5,8 @@ import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Assignment
+from CourseApp.models import Course  # Import the Course model from the CourseApp
+from datetime import datetime
 
 @csrf_exempt  # Only for development; remove in production and use proper authentication.
 def add_assignment(request):
@@ -30,5 +32,37 @@ def add_assignment(request):
 
         except json.JSONDecodeError:
             return JsonResponse({'error': 'Invalid JSON data'}, status=400)
+
+    return JsonResponse({'error': 'Invalid request method'}, status=405)
+
+
+@csrf_exempt  # Only for development; remove in production and use proper authentication.
+def get_assignments_for_course(request, course_id, enroll_date):
+    if request.method == 'GET':
+        try:
+            # Parse the enroll date as a datetime object
+            enroll_date = datetime.strptime(enroll_date, '%Y-%m-%d')
+
+            # Retrieve assignments for the course where start_date > enroll_date
+            assignments = Assignment.objects.filter(course_id=course_id, start_date__gt=enroll_date)
+            assignments_data = []
+
+            for assignment in assignments:
+                assignment_data = {
+                    'id': assignment.id,
+                    'course': assignment.course_id,
+                    'start_date': assignment.start_date.strftime('%Y-%m-%d %H:%M:%S'),
+                    'end_date': assignment.end_date.strftime('%Y-%m-%d %H:%M:%S'),
+                    'title': assignment.title,
+                    'description': assignment.description,
+                }
+                assignments_data.append(assignment_data)
+
+            return JsonResponse(assignments_data, safe=False)
+
+        except ValueError:
+            return JsonResponse({'error': 'Invalid date format. Use YYYY-MM-DD.'}, status=400)
+        except Assignment.DoesNotExist:
+            return JsonResponse([], safe=False)
 
     return JsonResponse({'error': 'Invalid request method'}, status=405)
